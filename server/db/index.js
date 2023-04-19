bf3b8c5fb726bfc5ed0de0d97b447425e4901779
Andrew Dooley <andrewdooley@gmail.com>
2023-04-19 13:01:57 -0400

    tryingassociations

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,27 @@
 //this is the access point for all things database related!
 
-const db = require('./db');
+const db = require("./db");
 
-const User = require('./models/User');
-const Product = require('./models/Product');
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Cart = require("./models/Cart");
+const CartItems = require("./models/CartItems");
 
 //associations could go here!
+CartItems.belongsTo(Product);
+User.hasMany(Cart);
+Cart.hasMany(CartItems);
+Product.belongsToMany(Cart, { through: "cartItem" });
+
+// CartItems.belongsTo(Cart);
+// Cart.belongsTo(User, { foreignkey: "userid" });
 
 module.exports = {
   db,
   models: {
     User,
     Product,
+    Cart,
+    CartItems,
   },
 };
