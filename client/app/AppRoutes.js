80f9095ef7eab85b388efc4526c2ce5befafaf82
Andrew Dooley <andrewdooley@gmail.com>
2023-04-19 16:23:19 -0400

    singlecomponentstarted

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import Home from "../features/home/Home";
+import { me } from "./store";
 
 /**
  * COMPONENT
@@ -38,6 +38,7 @@ const AppRoutes = () => {
             path="/signup"
             element={<AuthForm name="signup" displayName="Sign Up" />}
           />
+          <Route path="/product/:id" element={<SingleProduct />} />
         </Routes>
       )}
     </div>
