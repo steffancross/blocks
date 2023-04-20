3b78618a992f26bb00bb72475fefeb9c1ac481ee
Steffan Cross <steffancross@berkeley.edu>
2023-04-20 09:34:33 -0700

    when creating a user, a cart is created and assigned to them

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,21 @@
 //this is the access point for all things database related!
 
-const db = require("./db");
+const db = require('./db');
 
-const User = require("./models/User");
-const Product = require("./models/Product");
-const Cart = require("./models/Cart");
-const CartItems = require("./models/CartItems");
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Cart = require('./models/Cart');
+const CartItems = require('./models/CartItems');
 
 //associations could go here!
 CartItems.belongsTo(Product);
+
+Cart.belongsTo(User);
 User.hasMany(Cart);
+
 Cart.hasMany(CartItems);
-Product.belongsToMany(Cart, { through: "joinProductToCart" });
+
+Product.belongsToMany(Cart, { through: 'joinProductToCart' });
 
 // CartItems.belongsTo(Cart);
 // Cart.belongsTo(User, { foreignkey: "userid" });
