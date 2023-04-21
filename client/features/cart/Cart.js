ccbab02c6533463ebe99a92854398fe2e2e44749
Andrew Dooley <andrewdooley@gmail.com>
2023-04-21 10:55:35 -0400

    localstoragefixed

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-//pulling the products in localStorage to render non-logged in users current products in cart
-const products = Object.values(localStorage).map((products) =>
-  JSON.parse(products)
-);
-
-//remove from cart by targeting id key
-const removeFromCart = (product) => {
-  localStorage.removeItem();
-  location.reload();
-};
-
 const Cart = () => {
+  //create empty products array
+  const [products, setProducts] = useState([]);
+
+  //filter out tokens from local storage and then setProducts to the parsed key/value from the local storage
+  useEffect(() => {
+    let localProducts = Object.entries(localStorage).filter(
+      ([key]) => key !== "token"
+    );
+
+    setProducts(
+      localProducts.map(([key, value]) => ({
+        key,
+        ...JSON.parse(value),
+      }))
+    );
+  }, []);
+
+  //need an async function here so that sends the products to the cart via post thunk?
+  const completePurchase = () => {
+    console.log(products);
+  };
+
+  //remove from cart by targeting product key which was added in the useEffect
+  const removeFromCart = (product) => {
+    localStorage.removeItem(product.key);
+    location.reload();
+  };
+
   return (
     //if logged in show cart which is pulling from state/db
 
@@ -23,6 +40,8 @@ const Cart = () => {
         products.map((product) => (
           <div className="product" key={product.id}>
             <h3>Product: {product.name}</h3>
+            <h3>{`$${product.price}`}</h3>
+            <h3>Quantity: </h3>
             <button
               id="remove-from-cart"
               onClick={() => {
@@ -36,6 +55,14 @@ const Cart = () => {
       ) : (
         <div>You have no items in your cart</div>
       )}
+      <button
+        id="complete-purchase"
+        onClick={() => {
+          completePurchase();
+        }}
+      >
+        Complete your purchase
+      </button>
     </div>
   );
 };
