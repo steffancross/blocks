12e47ce39f9fc7b61569e9bfa937cb69f7d490ff
Luis Samboy <luissamboy@icloud.com>
2023-04-21 12:15:24 -0700

    Completed requireToken in server/api/gatekeepingMiddleware.js. Unable to prevent non-admins from viewing all user info

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -1,13 +1,14 @@
 const {
   models: { User },
 } = require("../db");
-//Will store all of our functions that will act as middleware between our reqeusts and response
 
+//Will store all of our functions that will act as middleware between our requests and response
 const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const user = await User.findByToken(token);
     req.user = user;
+    next();
   } catch (e) {
     next(e);
   }
