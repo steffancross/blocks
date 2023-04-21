0edabfff633e7bd27bd36935ad9b76296501efa5
Luis Samboy <luissamboy@icloud.com>
2023-04-21 10:37:13 -0700

    Changed server/auth/index.js so clients no longer allowed to make themselves admin

--- server/auth/index.js
+++ server/auth/index.js
@@ -15,7 +15,10 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/signup", async (req, res, next) => {
   try {
-    const user = await User.create(req.body);
+    //Clients are prevented from creating admin account
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+
     res.send({ token: await user.generateToken() });
   } catch (err) {
     if (err.name === "SequelizeUniqueConstraintError") {
