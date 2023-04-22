145aa819d3374e9b4dbd094446b1705bd3794a48
Steffan Cross <steffancross@berkeley.edu>
2023-04-22 13:51:32 -0700

    updated cart api POST route. if a user adds to cart, a new row is created in cartitems. however now if a user adds the same item to their cart, instead of a new row being created, the quantity is updated instead of another row being created

--- server/api/carts.js
+++ server/api/carts.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { models } = require("../db");
+const { models } = require('../db');
 const { Product, User, Cart, CartItems } = models;
 
-router.get("/:id/cart", async (req, res, next) => {
-  try {
-    const cartItems = await CartItems.findAll();
-    res.send(cartItems);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get("/:id/cart", async (req, res, next) => {
+//   try {
+//     const cartItems = await CartItems.findAll();
+//     res.send(cartItems);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 // Add Item to Cart
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const { userId, productId } = req.body;
 
@@ -25,16 +25,30 @@ router.post("/", async (req, res, next) => {
       },
     });
 
-    const product = await Product.findOne({ where: { id: productId } });
-
-    // create cart item
-    await CartItems.create({
-      quantity: 1,
-      cartId: cart.id,
-      productId: product.id,
+    // see if cartItem for that user/cart already exists
+    let cartItem = await CartItems.findOne({
+      where: {
+        cartId: cart.id,
+        productId: productId,
+      },
     });
 
-    res.send("cart updated");
+    if (cartItem) {
+      cartItem.quantity += 1;
+      await cartItem.save();
+    } else {
+      // find product
+      const product = await Product.findOne({ where: { id: productId } });
+
+      // create cart item
+      await CartItems.create({
+        quantity: 1,
+        cartId: cart.id,
+        productId: product.id,
+      });
+    }
+
+    res.send('cart updated');
   } catch (err) {
     next(err);
   }
