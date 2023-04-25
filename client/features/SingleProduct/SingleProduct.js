f6b89bbdd5509a52b16806693ed93b96a3dfb40c
Andrew Dooley <andrewdooley@gmail.com>
2023-04-25 15:38:57 -0400

    localcartdecreasequantityworking

--- client/features/SingleProduct/SingleProduct.js
+++ client/features/SingleProduct/SingleProduct.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import {
   fetchSingleProductAsync,
   selectSingleProduct,
   addProductToCartAsync,
-} from './SingleProductSlice';
+} from "./SingleProductSlice";
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
@@ -13,20 +13,29 @@ const SingleProduct = () => {
 
   const singleProduct = useSelector(selectSingleProduct);
   const user = useSelector((state) => state.auth.me);
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
   useEffect(() => {
     dispatch(fetchSingleProductAsync(id));
   }, [dispatch]);
 
   const addToCart = (singleProduct) => {
-    
-    const userIdToSend = user.id;
-    dispatch(
-      addProductToCartAsync({
-        userId: userIdToSend,
-        productId: singleProduct.id,
-      })
-    );
+    if (isLoggedIn) {
+      const userIdToSend = user.id;
+      dispatch(
+        addProductToCartAsync({
+          userId: userIdToSend,
+          productId: singleProduct.id,
+        })
+      );
+    } else {
+      //check for existing products in localStorage with getItem, if so, push new product to an array and set it to LS
+      // if not, create empty array, push product and set to LS
+
+      let products = JSON.parse(localStorage.getItem("products")) || [];
+      products.push(singleProduct);
+      localStorage.setItem("products", JSON.stringify(products));
+    }
   };
 
   return (
@@ -35,7 +44,7 @@ const SingleProduct = () => {
         <img
           src={singleProduct.image}
           alt="just an image"
-          style={{ width: '300px' }} //temporary in-line styling
+          style={{ width: "300px" }} //temporary in-line styling
         />
         <h3>{singleProduct.name}</h3>
         <h3>{`$${singleProduct.price}`}</h3>
